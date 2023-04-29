8d31006d4e44718dc0b97df289c5a4b77e010949
Ray Warner <raywarner@Rays-MBP.lan>
2023-04-29 15:38:54 -0400

    initial files

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'c60c9701863f4f3c9c10d6c60cbb292d'
+});
+
+const handleAPICall = (req, res) => {
+    app.models.predict('face-detection', req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleAPICall
+};
